13990f0c180932a9fc3e7cd16d5c4748a08ee296
Alen Pepa <xalenpepa@gmail.com>
2026-04-01 14:46:06 -0700

    Add files via upload

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+/* COPYRIGHT ALEN PEPA */
+import { ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
